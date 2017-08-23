6fce8bbbd62397561f450729770d732d390d5a2d
in0cc1.inoue <k.prfm19@gmail.com>
2017-08-24 00:50:29 +0900

    add .gitignore

---
+++ realmTest01/TextData.swift
@@ -0,0 +1,16 @@
+//
+//  textData.swift
+//  realmTest01
+//
+//  Created by Wataru Inoue on 2017/08/24.
+//  Copyright © 2017年 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TextData: Object {
+    dynamic var text = ""
+    dynamic var date: Date = Date()
+
+}
