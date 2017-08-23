91e52c079e6d9aba1dfe672556944c11a82dab16
in0cc1.inoue <k.prfm19@gmail.com>
2017-08-23 23:32:52 +0900

    tableViewのcellにとりあえずString配列の文字を出力

--- realmTest01/ViewController.swift
+++ realmTest01/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var textArray: [String] = ["あ", "い", "うえ"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +28,20 @@ class ViewController: UIViewController {
 
 }
 
+// ViewControllerを拡張(tableViewに関するコードをまとめるため)
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = textArray[indexPath.row]
+        return cell
+    }
+    
+}
